52533f4cb8317eddbf1103f8aa83ed25d2dbdfa2
Pulkit-gif <69722790+Pulkit-gif@users.noreply.github.com>
2021-09-16 23:34:24 +0530

    Add files via upload

---
+++ simpletemplate.py
@@ -0,0 +1,8 @@
+letter = ''' dear <|name>
+            you are selected
+               <|date>'''
+name=input("enter name")
+date=input("enter date")
+letter=letter.replace("<|name>",name)
+letter=letter.replace("<|date>",date)
+print(letter)
